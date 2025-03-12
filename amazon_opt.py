d9ec795614bab14512c3ad6336500dd6722cf534
Kim <stein.kim.a@gmail.com>
2025-03-12 13:42:25 -0700

    Model, hub, focus, center definitions.

--- amazon_opt.py
+++ amazon_opt.py
@@ -1,1 +1,24 @@
+# Import necessary packages
+from pulp import *
+import pandas as pd
+import numpy as np
 
+# Define model
+model = LpProblem('Minimum Distribution Cost Model', LpMinimize)
+
+# Define types of distribution locations
+hub = ['CVG', 'AFW']
+focus = ['Leipzig', 'Hyderabad', 'San Bernardino']
+center = ['Paris', 'Cologne', 'Hanover', 'Bangalore', 'Coimbatore', 'Delhi', 
+          'Mumbai', 'Cagliari', 'Catania', 'Milan', 'Rome', 'Katowice', 
+          'Barcelona', 'Madrid', 'Castle Donington', 'London', 'Mobile', 
+          'Anchorage', 'Fairbanks', 'Phoenix', 'Los Angeles', 'Ontario', 
+          'Riverside', 'Sacramento', 'San Francisco', 'Stockton', 'Denver', 
+          'Hartford', 'Miami', 'Lakeland', 'Tampa', 'Atlanta', 'Honolulu', 
+          'Kahului/Maui', 'Kona', 'Chicago', 'Rockford', 'Fort Wayne', 
+          'South Bend', 'Des Moines', 'Wichita', 'New Orleans', 'Baltimore', 
+          'Minneapolis', 'Kansas City', 'St. Louis', 'Omaha', 'Manchester', 
+          'Albuquerque', 'New York', 'Charlotte', 'Toledo', 'Wilmington', 
+          'Portland', 'Allentown', 'Pittsburgh', 'San Juan', 'Nashville', 
+          'Austin', 'Dallas', 'Houston', 'San Antonio', 'Richmond', 
+          'Seattle/Tacoma', 'Spokane']
